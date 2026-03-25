0b8c8225f4b4ccfa8eb3ddeb41df0ede908a81f0
shwetha-17 <shwethamgr17@gmail.com>
2026-03-25 16:04:49 +0530

    alt updated

--- src/components/saleimg.js
+++ src/components/saleimg.js
@@ -3,7 +3,7 @@ import saleimg from '../assets/images/saleimg.jpg';
 function Saleimg() {
     return (
         <div class="sale-image">
-            <img src={saleimg} alt="sale image"></img>
+            <img src={saleimg} alt="sale"></img>
                 <div class="sale-image__offer">
                     <h2>Great offer!!</h2>
                     <p><strong>Hurry up!!</strong> The biggest sale is on liive.. you can purchase any course for just 499 only.</p>
